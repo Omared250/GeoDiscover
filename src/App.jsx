bdbb72442290fdfd0be5aa57e70d2113574175ea
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-08-31 14:04:18 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Banner } from "./components/Banner"
+import { AppRouter } from "./router/AppRouter"
 
 function App() {
 
   return (
     <div>
-      <Banner />
+      <AppRouter />
     </div>
   )
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+import { Search } from "./Search";
 
 export const Banner = () => {
     return (
         <section className="banner">
-            <div>
+            <div className="inner">
                 <h1>THIS IS GEODISCOVER</h1>
                 <p>
                     Discover the world like never before with GeoDiscover! Our intuitive web app provides detailed insights and captivating
@@ -10,7 +12,9 @@ export const Banner = () => {
                     and embark on a virtual journey across the globe with GeoDiscover.
                 </p>
             </div>
-            <button type="button" class="btn btn-light">Start Now!</button>
+            <Link to="search">
+                <button type="button" className="btn btn-light">Start Now!</button>
+            </Link>
         </section>
     );
 }
