1272d24c99d66bb3773e968e09aeeb167a5fb870
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-08-31 23:18:37 +0200

    fetching data from API

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react"
+import getCountryByName from "../api/countryApi";
 
 export const Search = () => {
+    const [ inputValue, setInputValue ] = useState("");
+
+    const onInputChange = (e) => {
+        e.preventDefault();
+        setInputValue(e.target.value.toLowerCase().trim());
+    }
+
+    const onSearchSubmit = (e) => {
+        e.preventDefault();
+
+        getCountryByName(inputValue);
+    };
+
     return(
-        <div>
-            <h1>HELLO</h1>
-        </div>
+        <>
+            <div className="row search">
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <hr />
+                    <form onSubmit={ onSearchSubmit } aria-label="form">
+                        <input 
+                            type="text"
+                            placeholder="Search a country"
+                            className="form-control"
+                            autoComplete="off"
+                            value={ inputValue }
+                            onChange={ onInputChange }
+
+                        />
+                        <button className="btn btn-outline-primary mt-2">
+                            search
+                        </button>
+                    </form>
+                </div>
+                <div className="col-7">
+
+                    <h4>Results</h4>
+                    <hr />
+
+                    <div>
+
+                    </div>
+
+                </div>
+            </div>
+        </>
     )
 }
